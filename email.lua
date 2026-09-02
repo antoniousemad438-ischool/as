ce8cc962c8e71098dc756beea89563cfbfd1302e
antoniousemad438-ischool <antoniousemad438@gmail.com>
2026-09-02 19:33:44 +0300

    Add files via upload

---
+++ email.lua
@@ -0,0 +1,15 @@
+print ("Code Verse")
+print (" enter your name")
+local link_github = "https://github.com/antoniousemad438-ischool/as"
+local link_yt = "https://www.youtube.com/@code_verse1"
+name= io.read()
+print ("enter a strong password")
+password= io.read()
+domain= "@cv.com"
+if name=="cv user" then
+    print ("hi codeverse user thanks for emailing we love you ")
+    print ("for github"..link_github)
+else
+    print ("subiscribe here "..link_yt)
+end
+print (name..domain)
